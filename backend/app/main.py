50297da3b3e9c6783001c07393dae48c68fe447b
Steve <shyam.evans01@gmail.com>
2026-06-12 22:27:04 +0530

    Temporary open CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173" ,
-        "https://steven-summarizer.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
