43ff4f929bbe2d2ebed4f03cdf2771c33d761f53
Steve <shyam.evans01@gmail.com>
2026-06-12 18:09:55 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173" ,
-        "steven-summarizer.vercel.app"
+        "https://steven-summarizer.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
